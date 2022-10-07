f710f7f2b6c9a4fcdbf6d100c1a5245913941f39
Andrew Reynolds <andrew_reynolds@mac.com>
2022-10-07 13:19:55 -0700

    updated fortuneTeller function

--- lab7.js
+++ lab7.js
@@ -12,13 +12,13 @@ const nullOrEmptyString = (inputVariable) => {
   }
 };
 const fortuneTeller = (jobTitle, loc, partnerName, numChild) => {
-  if (nullOrEmptyString(jobTitle) && nullOrEmptyString(jobTitle)) {
-    if (partnerName.trim().length && numChild.trim().length) {
+  if (nullOrEmptyString(jobTitle) && nullOrEmptyString(loc)) {
+    if (nullOrEmptyString(partnerName) && nullOrEmptyString(numChild)) {
       console.log(
         `You will be a ${jobTitle} in ${loc}, and married to ${partnerName} with ${numChild} kids.`
       );
       output = `You will be a ${jobTitle} in ${loc}, and married to ${partnerName} with ${numChild} kids.`;
-    } else if (partnerName.trim().length) {
+    } else if (nullOrEmptyString(partnerName)) {
       console.log(
         `You will be a ${jobTitle} in ${loc}, and married to ${partnerName} with no kids.`
       );
@@ -33,6 +33,24 @@ const fortuneTeller = (jobTitle, loc, partnerName, numChild) => {
     console.log("test");
     error = true;
   }
+
+  if (!error) {
+    testTwo.innerHTML = `<p>${output}</p>`;
+  } else {
+    let errorDiv = document.createElement("div");
+    let errorParagraph = document.createElement("p");
+    let cover = document.createElement("div");
+
+    errorParagraph.textContent = `Error, you must enter at both a "Job Title" and "Where you're located". Please reset and try again!`;
+    errorDiv.appendChild(errorParagraph);
+    errorDiv.className = "error";
+    cover.className = "cover";
+    document.querySelector("body").appendChild(errorDiv);
+    document.querySelector("body").appendChild(cover);
+    document.querySelector(".cover").addEventListener("click", () => {
+      location.reload();
+    });
+  }
 };
 
 let jobTitle = prompt("What is your Job Title: ");
@@ -40,25 +58,6 @@ let userLocation = prompt("Where are you located: ");
 let partnerName = prompt(`What is your Partner's name: `);
 let numChild = prompt("How many children do you have: ");
 
-fortuneButton.addEventListener(
-  "click",
+fortuneButton.addEventListener("click", () =>
   fortuneTeller(jobTitle, userLocation, partnerName, numChild)
 );
-
-if (!error) {
-  testTwo.innerHTML = `<p>${output}</p>`;
-} else {
-  let errorDiv = document.createElement("div");
-  let errorParagraph = document.createElement("p");
-  let cover = document.createElement("div");
-
-  errorParagraph.textContent = `Error, you must enter at both a "Job Title" and "Where you're located". Please reset and try again!`;
-  errorDiv.appendChild(errorParagraph);
-  errorDiv.className = "error";
-  cover.className = "cover";
-  document.querySelector("body").appendChild(errorDiv);
-  document.querySelector("body").appendChild(cover);
-  document.querySelector(".cover").addEventListener("click", () => {
-    location.reload();
-  });
-}
